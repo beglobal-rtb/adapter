115e44dc0ba868f057a2715311cf8c02793f6638
Sonu Kumar Soni <developer@rtbdemand.com>
2023-07-07 11:28:37 +0530

    Version 1.0.0 released

--- app/src/main/java/com/rtb/bemonetise/MainActivity.kt
+++ app/src/main/java/com/rtb/bemonetise/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.rtb.bemonetise
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.admanager.AdManagerAdRequest
 import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.google.android.gms.ads.rewardedinterstitial.RewardedInterstitialAd
 import com.rtb.bemonetise.databinding.ActivityMainBinding
 
@@ -16,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
         loadAd()
+        loadInterstitial()
     }
 
     private fun loadAd() {
@@ -23,6 +27,20 @@ class MainActivity : AppCompatActivity() {
         binding.bannerAd.loadAd(adRequest)
     }
 
+
+    private fun loadInterstitial(){
+        val adRequest = AdManagerAdRequest.Builder().build()
+        InterstitialAd.load(this, "/21952429235,21952429235/be_sun_com.livecameracctv.videocamerarecorder_appinterstitial1", adRequest, object: InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(p0: LoadAdError) {
+                Log.d("Ads", "onAdFailedToLoad: $p0")
+            }
+
+            override fun onAdLoaded(p0: InterstitialAd) {
+                super.onAdLoaded(p0)
+                Log.d("Ads", "onAdLoaded:")
+            }
+        })
+    }
 
 
     override fun onResume() {
